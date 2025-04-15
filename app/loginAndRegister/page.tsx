b7911048ef3a9e0da080c75cb5204d18885c4176
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-04-15 17:31:20 +0200

    ajout code form login coté front

--- app/loginAndRegister/page.tsx
+++ app/loginAndRegister/page.tsx
@@ -1,6 +1,6 @@
 // Code pour les imports : ----------------------------------------------------------------------------------------------------------------------//
 "use client" // composant éxécuté coté client
-
+import { useRef } from "react"; 
 
 
 
@@ -8,7 +8,60 @@
 
 
 // Code pour retourner le composant Login & Register : ------------------------------------------------------------------------------------------//
-export default function page() {
+export default function pageLoginRegister() {
+
+
+// Code pour gérer la logique du form LOGIN : ----------------------------------------------------------------------------------------------------//
+  const emailLoginRef =  useRef<HTMLInputElement>(null)
+  const mdpLoginRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault(); 
+
+    const email = emailLoginRef.current?.value
+    const mdp = mdpLoginRef.current?.value
+
+    console.log(email, mdp)
+
+    try {
+      const response = await fetch ("api/auth/login", {
+        method: "POST", 
+        headers: {
+          'Content-Type': "application/json"
+        }, 
+        body: JSON.stringify({email, mdp})
+      })
+  
+      if(!response.ok) {
+        throw new Error("Identifiants invalides")
+       }
+  
+       const data = await response.json(); 
+       alert("Vous etes bien connecté !");
+
+       // ici il, faudra qu'on reset le formulaire: 
+       const form = e.currentTarget as HTMLFormElement 
+       form.reset(); 
+       
+    } catch (error) {
+      console.error("Erreur serveur: ", error); 
+      alert ("Echec lors de la connexion , verifiez vos identifiants")
+    }
+    
+
+
+      
+
+
+  }
+
+
+
+
+
+// Code pour gérer la logique du form REGISTER : ----------------------------------------------------------------------------------------------------//
+
+
   return (
     <div
         className="flex flex-col items-center justify-center bg-[url('/fond/fondArtCommerceBeige.png')] bg-cover bg-center h-[100vh] "> 
@@ -19,26 +72,31 @@ export default function page() {
                     className="font-boogaloo text-2xl text-slate-700 mt-2 ">
                       Se Connecter: 
                 </h2>
-                        <form className="flex flex-col font-rubik p-4">
+                        <form className="flex flex-col font-rubik p-4"
+                              onSubmit={handleSubmit}>
                     
                         {/* Champ email:  */}
-                        <label htmlFor=""
+                        <label htmlFor="emailLogin"
                             className="text-center">
                                Adresse email:
                         </label>
                         <input 
                             className="border-2 border-slate-200 rounded-md mb-2"
                             type="text"
+                            id="emailLogin"
+                            ref={emailLoginRef}
                             />
 
                         {/* Champ mot de passe:  */}
-                        <label htmlFor=""
+                        <label htmlFor="mdpLogin"
                             className="text-center">
                                Mot de passe:
                         </label>
                         <input 
                             className="border-2 mb-4 border-slate-200 rounded-md"
                             type="text"
+                            id="mdpLogin"
+                            ref={mdpLoginRef}
                             />
                         
                         <button
