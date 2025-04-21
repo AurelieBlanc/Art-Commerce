063d0aaeda5035d0b513c1982fbd6cba5c6f136a
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-04-21 19:17:05 +0200

    debut creation checkAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,50 @@
 // Code pour les imports : ---------------------------------------------------//
+"use client"
+import { useEffect } from "react"; 
+import Cookies from "js-cookie"; 
+
 
 
 
 
 export default function Home() {
 //Code pour lancer un appel API au montage du composant pour faire un checkAuth : //
+
+useEffect(() => {
+
+  async function checkAuth(){
+
+    try {
+
+      const response = await fetch ("/api/auth/checkAuth", {
+        method: "POST", 
+        credentials: "include", 
+        headers : {
+          "Content-Type" : "application/json", 
+          "x-csrf-token": Cookies.get("csrfToken") || "", //si undefined on renverra une chaine vide pour eviter les erreurs
+        }
+      }); 
+ 
+      if(!response.ok) {
+        throw new Error ("reponse Auth erreur")
+      }
+
+      
+      const data = await response.json();
+      console.log("message : ") // A ADAPTER SELON MON BACK
+
+      // ET BIEN METTRE A JOUR un fichier ZUSTAND avec user authenticated or not
+      // donc creation d'un fichier avec un STORE GLOBAL à la racine du projet
+      
+
+    } catch(error) {
+      console.error("La verification de l'auth a échoué : ", error)
+    }
+  }
+
+  checkAuth(); // on appelle la fonction pour l'éxécuter
+
+}, []) // se lance au montage du composant
 
   return (
     <div
---
+++ pages/api/auth/checkAuth.ts
@@ -0,0 +1,60 @@
+// Code pour les imports : -----------------------------------------------------//
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt  from "jsonwebtoken";
+import * as cookie from "cookie";
+import { PrismaClient } from "@prisma/client"; 
+
+const prisma = new PrismaClient(); 
+const SECRET_KEY = process.env.JWT_SECRET; 
+
+
+
+
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+    if(req.method !== "POST") {
+        return res.status(405).json({ message: "requete HTTP non autorisée "})
+    }
+
+// Code pour recupérer et lire les cookies : --------------------------------------------- //
+    
+try {
+    const cookies = cookie.parse(req.headers.cookie || ""); 
+    console.log("cookies : ", cookies)
+
+    const authToken = cookies.authToken; 
+    console.log("token : ", authToken ); 
+
+  
+    const csrfToken = cookies.csrfToken; 
+    console.log("tokenCSRF ", csrfToken); 
+
+    if(!authToken || !csrfToken ) {
+    return res.status(401).json({ message: "Token(s) manquant(s)" })
+    }
+
+    if(!SECRET_KEY) {
+        throw new Error ("la clé sécrète n'est pas correcetement définie")
+    }
+
+    const decoded = jwt.verify(authToken, SECRET_KEY); 
+    
+
+    if(decoded && typeof decoded !== "string") {
+        
+        console.log("decoded", decoded.role, decoded.id, decoded.email ); 
+        return res.status(200).json({ isAuthenticated : true});
+        
+        // ICI on rajoutera un token CSRF 
+
+
+    } else {
+        return res.status(401).json({ message: "Non Autorisé"})
+    }
+
+} catch(error) {
+    console.error("Erreur lors de la vérif de la connexion de l'user ou l'admin", error); 
+    return res.status(500).json({ isAuthenticated: false })
+}
+
+}
