cb803fd1f6cc04f57c259ec938adf8de606be8c5
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-04-21 17:59:08 +0200

    ajout modif avant creation nvelle branche

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+// Code pour les imports : ---------------------------------------------------//
+
+
 
 
 export default function Home() {
+//Code pour lancer un appel API au montage du composant pour faire un checkAuth : //
+
   return (
     <div
       className="bg-[url('/fond/fondArtCommerceBeige.png')] bg-cover bg-center h-[50vh] "> HELLO PAGE D'ACCUEIL où l'on affichera les produits 
