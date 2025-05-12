ee6b52578bbfdaf3af288ade793b855a1819e181
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-05-12 13:52:20 +0200

    finition page success avec suppression du cookie et nettoyage du panier

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,13 +1,59 @@
 // Code pour les imports : --------------------------------------------------- //
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import { IoIosArrowDroprightCircle } from "react-icons/io"; // icone fleche : <IoIosArrowDroprightCircle />
-
-
-
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { deletePanier } from "../../utils/panierCookie"; 
 
 
 export default function page() {
+
+// Code pour les outils et les states : 
+const searchParams = useSearchParams(); 
+
+const idCommande = searchParams?.get("idCommande"); 
+
+
+
+
+// Code pour récupérer la commande en paramètre 
+useEffect(()=> {
+
+  async function getOrderAndDeleteBasket() {
+
+    try {
+      const response = await fetch (`/api/commandes/getOneOrder/${idCommande}`, {
+        method:"GET"
+      })
+
+      if (!response.ok) {
+        throw new Error("réponse appel commande erreur")
+      }
+
+      const data = await response.json();
+      console.log("qu y a t'il dans data ? ", data) 
+
+      const statut = data.order.statut 
+      console.log("qu y a til dans statut ? ", statut); 
+
+      if(statut === "PAYEE, EN COURS DE PREPARATION") {
+        deletePanier(); 
+      }
+
+    } catch(error) {
+      console.error("La recupération de la commande a échoué", error)
+    }
+  }
+
+  getOrderAndDeleteBasket(); 
+
+}, [])
+
+
+
+
   return (
     <div
         className="bg-[url('/fond/fondArtCommerceBeige.png')] bg-cover bg-center flex flex-col items-center justify-center">
