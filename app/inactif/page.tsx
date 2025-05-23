6dac2ee7ae97a4852e52ea7e2167c09af7806d62
Aurélie Blanc <aurelie.blanc@oclock.school>
2025-05-23 18:23:45 +0200

    finition page inactive products

--- app/inactif/page.tsx
+++ app/inactif/page.tsx
@@ -2,7 +2,8 @@
 "use client"
 
 import { useEffect, useState } from "react"; 
-
+import Image from "next/image";
+import Link from "next/link";
 
 
 
@@ -24,7 +25,9 @@ interface Produit {
 export default function page() {
 // Code pour déclarer les states locaux : ---------------------------- //
 
-const [ produits , setProduits ] = useState<Produit[]>([])
+const [ produits , setProduits ] = useState<Produit[]>([]); 
+
+const [ loading, setloading ] = useState<boolean>(false); 
 
 
 
@@ -51,7 +54,7 @@ useEffect(() => {
 
             setProduits(data)
 
-
+            setloading(true); 
 
         } catch(error) {
             console.error("erreur lors de la récupération des produits inactifs: ", error)
@@ -60,6 +63,8 @@ useEffect(() => {
     }; 
 
     getInactiveProducts(); 
+
+    
 
 }, [])
 
@@ -70,18 +75,60 @@ useEffect(() => {
 
 
 // Code pour retourner le composant JSX : ---------------------------- //
-  return (
+
+if(loading) {
+    return (
     <div
-        className="bg-[url('/fond/fondArtCommerceBeige.png')] bg-top bg-cover"> 
+        className="bg-[url('/fond/fondArtCommerceBeige.png')] bg-top bg-cover flex items-center justify-center"> 
 
         <div
-            className="containerProducts ">
+            className="containerProducts mt-6 mb-6">
 
                 {produits.map((elem) => (
                     <div
-                        className="w-[300px] h-[500px]">
-                            <p>{elem.nom}</p>
-                    </div>
+                        className="w-[300px] h-[500px] bg-blue-600 bg-opacity-10 border-white border-2 flex flex-col justify-center items-center rounded-md">
+
+                        <div
+                            className="">
+                                <Image
+                                src={elem.image}
+                                width={250}
+                                height={0}
+                                alt={elem.description}
+                                layout="intrinsic"
+                                />
+                        </div>
+
+                        <div
+                            className="nomProduct font-boogaloo text-xl mt-1">
+                             <p>{elem.nom}</p>
+                        </div>
+
+                        <div
+                            className="descriptionProduct text-sm ">
+                            <p>{elem.description}</p>
+                        </div>
+      
+                        <div
+                            className="prixProduct font-boogaloo text-xl ">
+                            <p>{elem.prix} €</p>
+                        </div>
+
+                        <div
+                            className="activeProduct text-red-600 font-boogaloo mt-4 text-xl font-bold">
+                            <p>{elem.is_active ? "Produit actif" : "Produit inactif"} </p>
+                        </div>
+
+                        <Link
+                            href={`/products/updateProduct/${elem.id_produit}`}>
+                        <button
+                            className="bg-slate-950 text-white border-slate-400 border-2 shadow-xl w-[150px] h-[40px] rounded-lg font-boogaloo mt-4">
+                            
+                                Le modifier
+                        </button>
+                        </Link>
+
+                     </div>
 
                 ))}
 
@@ -89,4 +136,15 @@ useEffect(() => {
 
     </div>
   )
+
+
+} else {
+    return (
+        <div
+            className="bg-[url('/fond/fondArtCommerceBeige.png')] bg-top bg-cover font-boogaloo text-2xl text-slate-900 pt-6 pb-10 text-center">
+                EN ATTENTE DE CHARGEMENT....
+        </div>
+    )
+}
+  
 }
